9e6404fad9a53791977052782e1ce8a6d05d8fca
andreyka101 <andrejkvasnin236@gmail.com>
2025-12-16 22:46:44 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,146 @@
+// import './style.css'
+
+
+console.log("hello TS");
+
+
+
+
+
+
+
+// let num = 5
+// num = 69
+// console.log(num);
+// const num2 = 5
+// // num2 = 69
+// console.log(num2);
+
+
+
+// alert("text")
+
+
+
+// console.log(typeof(0));
+// console.log(typeof(0.1));
+// console.log(typeof("344"));
+// console.log(typeof(true));
+// console.log(typeof(alert));
+// console.log(typeof(console));
+
+
+
+// let num_none1 = null
+// console.log(num_none1);
+
+
+// let num_none2
+// console.log(num_none2);
+
+
+// let num_none3 = 2 / 0
+// console.log(num_none3);
+
+
+// let num_none4 = 20 / "1a0"
+// console.log(num_none4);
+
+
+
+// let type_num1
+// let type_num1 : number
+// console.log(type_num1);
+// type_num1 = 200
+// console.log(type_num1);
+// type_num1 = "str"
+// console.log(type_num1);
+
+
+
+
+// let type_num2 : any = 'test'
+// // let type_num2 = 'test' as any
+// console.log(type_num2);
+// type_num2 = 200
+// console.log(type_num2);
+// type_num2 = "str"
+// console.log(type_num2);
+
+
+
+// let num_if = 2
+// if(num_if < 20){
+//     console.log("ok");
+// }
+
+
+// if(num_if > 20){
+//     console.log("ok");
+// }
+// else{
+//     console.log("not");
+// }
+
+
+// if(num_if > 20){
+//     console.log("ok");
+// }
+// else if(num_if < 20){
+//     console.log("else ok");
+// }
+// else{
+//     console.log("not");
+// }
+
+
+
+
+// let num_if_2 = 30
+// if(num_if_2 == "30"){
+//     console.log("ok");
+// }
+// else{
+//     console.log("not");
+// }
+
+
+// if(num_if_2 === "30"){
+//     console.log("ok");
+// }
+// else{
+//     console.log("not");
+// }
+
+
+
+
+// -
+// +
+// /
+// *
+// **
+// %
+
+
+// console.log(20/3);
+// console.log(20%3);
+
+
+
+
+alert("hello")
+// alert("hello")
+// alert("hello")
+
+
+let prom_num = prompt("num ?" , "6")
+console.log(prom_num);
+
+
+
+console.log(confirm("да / нет   ?"));
+
+
+
+
