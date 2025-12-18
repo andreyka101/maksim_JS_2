6539fab26ea146f035b95547bcb13b2d313f839d
andreyka101 <andrejkvasnin236@gmail.com>
2025-12-18 13:38:28 +0300

    цвцввццв

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,57 @@
 // import './style.css'
 
 
+
+
+// вывод данных
 console.log("hello TS");
 
 
 
+// typeof - возвращает тип данных
 
-
-
-
-// let num = 5
-// num = 69
-// console.log(num);
-// const num2 = 5
-// // num2 = 69
-// console.log(num2);
-
-
-
-// alert("text")
-
-
-
+// number
 // console.log(typeof(0));
+// number
 // console.log(typeof(0.1));
+// string
 // console.log(typeof("344"));
+// boolean
 // console.log(typeof(true));
+// function
 // console.log(typeof(alert));
+// object
 // console.log(typeof(console));
-
-
-
+// null - пустота
 // let num_none1 = null
 // console.log(num_none1);
-
-
+// undefined - переменная созданная пустой
 // let num_none2
 // console.log(num_none2);
-
-
+// Infinity - ошибка вычисления чисел
 // let num_none3 = 2 / 0
 // console.log(num_none3);
-
-
+// NaN - ошибка вычисления разных тип данных
 // let num_none4 = 20 / "1a0"
 // console.log(num_none4);
 
 
 
+// объявление переменной
+let type_num1 = 50
+// объявление пустой переменной
 // let type_num1
+// объявление пустой переменной с типом
 // let type_num1 : number
 // console.log(type_num1);
+// меняем переменную
 // type_num1 = 200
 // console.log(type_num1);
+// переменную с типом number нельзя менять на string
 // type_num1 = "str"
 // console.log(type_num1);
 
-
-
-
+// any - любой тип
 // let type_num2 : any = 'test'
 // // let type_num2 = 'test' as any
 // console.log(type_num2);
@@ -68,13 +61,20 @@ console.log("hello TS");
 // console.log(type_num2);
 
 
+// константу менять нельзя
+// const num_const = 5
+// num_const = 10
+// console.log(num_const);
 
+
+
+// условие
 // let num_if = 2
 // if(num_if < 20){
-//     console.log("ok");
+    //     console.log("ok");
 // }
 
-
+// иначе
 // if(num_if > 20){
 //     console.log("ok");
 // }
@@ -82,7 +82,7 @@ console.log("hello TS");
 //     console.log("not");
 // }
 
-
+// иначе если
 // if(num_if > 20){
 //     console.log("ok");
 // }
@@ -94,8 +94,7 @@ console.log("hello TS");
 // }
 
 
-
-
+// неточное сравнение
 // let num_if_2 = 30
 // if(num_if_2 == "30"){
 //     console.log("ok");
@@ -104,7 +103,7 @@ console.log("hello TS");
 //     console.log("not");
 // }
 
-
+// точное сравнение
 // if(num_if_2 === "30"){
 //     console.log("ok");
 // }
@@ -114,18 +113,15 @@ console.log("hello TS");
 
 
 
-
+//  арифметические вычисления
 // -
 // +
 // /
 // *
 // **
 // %
-
-
 // console.log(20/3);
 // console.log(20%3);
-
 
 
 
@@ -133,12 +129,11 @@ alert("hello")
 // alert("hello")
 // alert("hello")
 
-
+// сообщение prompt возвращает текст или None
 let prom_num = prompt("num ?" , "6")
 console.log(prom_num);
 
-
-
+// сообщение confirm возвращает boolean
 console.log(confirm("да / нет   ?"));
 
 
